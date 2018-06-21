566c7ab6228c4852cd4c46b7b1cab875c7f726d2
Shaun Kelly <Shaun@redskelly.local>
2018-06-20 23:59:20 -0400

    add color to vertices

--- src/graph.py
+++ src/graph.py
@@ -4,11 +4,18 @@ class Edge:
 
 
 class Vertex:
-    def __init__(self, value, **pos):
+    def __init__(self, value, color, **pos):
         self.value = value
-        self.color = 'white'
+        self.color = color
         self.pos = pos
         self.edges = []
+
+    # how to randomize vertex color?
+    # def update(self):
+    #     r = (self.color[0] + 1) % 256
+    #     g = (self.color[1] - 1) % 256
+    #     b = (self.color[2] + 1) % 256
+    #     self.color = [r, g, b]
 
 
 class Graph:
@@ -16,11 +23,11 @@ class Graph:
         self.vertexes = []
 
     def debug_create_test_data(self):
-        debug_vertex_1 = Vertex('p1', x=40, y=40)
-        debug_vertex_2 = Vertex('p2', x=140, y=140)
-        debug_vertex_3 = Vertex('p3', x=350, y=150)
-        debug_vertex_4 = Vertex('p4', x=300, y=400)
-        debug_vertex_5 = Vertex('p5', x=300, y=150)
+        debug_vertex_1 = Vertex('p1', 'skyblue', x=40, y=40)
+        debug_vertex_2 = Vertex('p2', 'blue', x=140, y=140)
+        debug_vertex_3 = Vertex('p3', 'green', x=350, y=150)
+        debug_vertex_4 = Vertex('p4', 'red', x=300, y=400)
+        debug_vertex_5 = Vertex('p5', 'brown', x=300, y=150)
 
         debug_edge_1 = Edge(debug_vertex_2)
         debug_vertex_1.edges.append(debug_edge_1)
